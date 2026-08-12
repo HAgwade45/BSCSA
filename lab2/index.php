9636a3955beff77b3c56f57ba29e0b82d47f0970
HAgwade45 <jaredlovesschool@gmail.com>
2026-08-12 09:50:24 +0800

    lab2 like larp2

---
+++ lab2/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bordalarp</title>
+</head>
+<body>
+    <h1>About me</h1>
+    <h5><marquee width="60%" direction="up" height="100px">Simp Jared D. Bordaje</marquee></h5>
+    <p>I like gaming and larping</p>
+    <img src="https://static.wikia.nocookie.net/umamusume/images/2/22/Manhattan_Cafe_%28Race%29.png/revision/latest?cb=20240728204249"></img>
+    <ul>
+        <li>3D Animation</li>
+        <li>Automata Theory and Formal Languages</li>
+        <li>Cloud Based Application Development</li>
+        <li>CS Elective 1 Technopreneurship</li>
+        <li>Game Programming 2 Advance Game Design and Development</li>
+        <li>GE Electives 3 (Gender and Society)</li>
+        <li>Spirituality in the Workplace</li>
+        <li>Understanding the Self</li>
+        
+    </ul>
+    <ol>
+        <li>Backribs</li>
+        <li>Pisang</li>
+    </ol>
+    <ul>
+        <li>https://github.com/HAgwade45</li>
+    </ul>
+</body>
+</html>
